c5820a688d7ad2f1eb4fa5019da6641238086a58
spoonyforkman <ryangtaylor@gmail.com>
2024-08-23 22:56:04 -0400

    More Chapters

--- server_Chapter7.js
+++ server_Chapter7.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
@@ -12,10 +11,22 @@ app.use(logger);
 
 // Cross Origin Resource Sharing
 // Add React server to whitelist
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) != -1 || !origin ) { // !origin for development
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
-// in other words, form data: 'content-type: application/x-www-form-urlcoded'
+// in other words, form data:
+// 'content-type: application/x-www-form-urlcoded'
 app.use(express.urlencoded({ extended: false }));
 
 // built-in middleware for json
@@ -23,9 +34,11 @@ app.use(express.json());
 
 // serve static files
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
